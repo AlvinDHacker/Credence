87c9268473bca90d49b0e4100b044b871f684d5c
ZaneFerns360 <zane.ferns360@gmail.com>
2024-01-17 19:29:57 +0530

    Co-authored-by: Jayden Colaco <jaydencolaco@users.noreply.github.com>

--- client/src/app/_components/VehicleForms.tsx
+++ client/src/app/_components/VehicleForms.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FileText, ChevronRight } from "lucide-react";
 
 const VehicleForms = () => {
   return (
-    <div>
-        <h1>Hello</h1>
+    <div className="mx-auto w-[80%]">
+      <h1 className="mb-5 text-2xl font-bold">Required steps</h1>
+      <div className="row-">
+        <p className="text-lg font-bold">Welcome User</p>
+        <p className="text-base font-light">
+          Here is what you need to do to set up your account.
+        </p>
+        <div className="flex flex-row gap-3">
+          <FileText />
+          <h1>Driving license</h1>
+          <p>Action </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default VehicleForms
+export default VehicleForms;
--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -1,12 +1,13 @@
 import Navbar from "../_components/Navbar";
 import Portfolio from "../_components/Portfolio";
+import GenerateQR from "../_components/GenerateQR";
 
 export default async function Home() {
-
   return (
     <>
-      <Navbar/>
-      <Portfolio/>
+      <Navbar />
+      <Portfolio />
+      <GenerateQR data={"hello"} />
     </>
   );
 }
