d4b530125c2f43371881a9447b1bc80cbff6022c
Joyvin <joymendonca710@gmail.com>
2024-01-19 09:38:06 +0530

    Product Verify

--- client/src/app/scan/page.tsx
+++ client/src/app/scan/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import StatusScanner from '../_components/StatusScanner'
+import React from "react";
+import StatusScanner from "../_components/StatusScanner";
+import Navbar from "../_components/Navbar";
 
 export default function page() {
-
   return (
-    <StatusScanner />
-  )
+    <div className="">
+      <Navbar />
+      <StatusScanner />
+    </div>
+  );
 }
